52cc7cb5354283ac8c3477fc90a66e4197ac1ad1
nurcity <nurbol.sakenov@outlook.com>
2021-11-14 00:27:23 +0100

    added windowed aggregation

--- src/main/java/com/nurbolsakenov/kafkastreaming/App.java
+++ src/main/java/com/nurbolsakenov/kafkastreaming/App.java
@@ -2,29 +2,21 @@ package com.nurbolsakenov.kafkastreaming;
 
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Locale;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.Grouped;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Printed;
-import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.*;
 import org.apache.log4j.PropertyConfigurator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public final class App {
-    private static final Logger logger = LoggerFactory.getLogger(App.class);
     public static final String INPUT_TOPIC = "word-count-input";
     public static final String OUTPUT_TOPIC = "word-count-output";
 
@@ -35,40 +27,29 @@ public final class App {
         config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         config.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+        config.put(StreamsConfig.BUFFERED_RECORDS_PER_PARTITION_CONFIG, 0);
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); 
-        config.put(ProducerConfig.ACKS_CONFIG, "all");
-        config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
-        config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        config.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-
-
-
 
         return config;
     }
 
     static void createWordCountStream(final StreamsBuilder builder) {
-        final KStream<String, String> source = builder.stream(INPUT_TOPIC, Consumed.with(Serdes.String(), Serdes.String()));
+        final KStream<String, String> source = builder.stream(INPUT_TOPIC,
+                Consumed.with(Serdes.String(), Serdes.String()));
 
-        final KTable<String, Long> counts = source
-                .peek((k,v) -> logger.info("Transformed event: {}", v))
+        final KTable<Windowed<String>, Long> test = source
                 .flatMapValues(value -> Arrays.asList(value.toLowerCase(Locale.getDefault()).split("\\W+")))
                 .groupBy((key, word) -> word, Grouped.with(Serdes.String(), Serdes.String()))
-                .count();
+                .windowedBy(TimeWindows.of(Duration.ofSeconds(1)))
+                .count(Materialized.as("test"));
+
+        test.toStream().map((key, value) -> KeyValue.pair(key.key(), key.key() + ": " + value.toString()))
+                .to(OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.String()));
 
         source.foreach((key, value) -> {
             System.out.println("(DSL): " + value);
         });
-
-        source.to(OUTPUT_TOPIC);
-
-        counts.toStream().print(Printed.toSysOut());
-
-        // need to override value serde to Long type
-        counts.toStream().to(OUTPUT_TOPIC); //Produced.with(Serdes.String(), Serdes.Long())
     }
 
     public static void main(final String[] args) {
