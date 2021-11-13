a45e2499c7c5e9872a46ab166d9cc0177e686da6
nurcity <nurbol.sakenov@outlook.com>
2021-11-13 22:01:06 +0100

    update

--- src/main/java/com/nurbolsakenov/kafkastreaming/App.java
+++ src/main/java/com/nurbolsakenov/kafkastreaming/App.java
@@ -1,60 +1,107 @@
 package com.nurbolsakenov.kafkastreaming;
 
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 import java.util.Arrays;
 import java.util.Locale;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Printed;
 import org.apache.kafka.streams.kstream.Produced;
 import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public final class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+    public static final String INPUT_TOPIC = "word-count-input";
+    public static final String OUTPUT_TOPIC = "word-count-output";
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        String log4jConfPath = "src/main/java/com/nurbolsakenov/resources/log4j.properties";
-        PropertyConfigurator.configure(log4jConfPath);
+    static Properties getStreamsConfig(final String[] args) {
+
         Properties config = new Properties();
         config.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafkastreaming");
         config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         config.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName()); 
-        
-        StreamsBuilder builder = new StreamsBuilder();
-        KStream<String, String> textLines = builder.stream("word-count-input");
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); 
+        config.put(ProducerConfig.ACKS_CONFIG, "all");
+        config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        config.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-        final KTable<String, Long> wordCounts = textLines
+
+
+
+        return config;
+    }
+
+    static void createWordCountStream(final StreamsBuilder builder) {
+        final KStream<String, String> source = builder.stream(INPUT_TOPIC, Consumed.with(Serdes.String(), Serdes.String()));
+
+        final KTable<String, Long> counts = source
+                .peek((k,v) -> logger.info("Transformed event: {}", v))
                 .flatMapValues(value -> Arrays.asList(value.toLowerCase(Locale.getDefault()).split("\\W+")))
-                .groupBy((key, value) -> value)
+                .groupBy((key, word) -> word, Grouped.with(Serdes.String(), Serdes.String()))
                 .count();
-        
-        textLines.foreach( 
-            (key, value) -> {
-                System.out.println("(DSL) Hello, " + value);
-            });
 
-                
-        wordCounts.toStream().to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
+        source.foreach((key, value) -> {
+            System.out.println("(DSL): " + value);
+        });
 
-        
-        KafkaStreams streams = new KafkaStreams(builder.build(), config);
-        streams.start();
+        source.to(OUTPUT_TOPIC);
 
-        // print the topology
-        System.out.println(streams.toString());
+        counts.toStream().print(Printed.toSysOut());
 
-        // add shutdown hook to stop the application gracefully
-        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+        // need to override value serde to Long type
+        counts.toStream().to(OUTPUT_TOPIC); //Produced.with(Serdes.String(), Serdes.Long())
+    }
 
+    public static void main(final String[] args) {
+        String log4jConfPath = "src/main/java/com/nurbolsakenov/resources/log4j.properties";
+        PropertyConfigurator.configure(log4jConfPath);
+        final Properties props = getStreamsConfig(args);
+
+        final StreamsBuilder builder = new StreamsBuilder();
+        createWordCountStream(builder);
+
+        Topology topology = builder.build();
+
+        System.out.println(topology.describe());
+        final KafkaStreams streams = new KafkaStreams(topology, props);
+
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-wordcount-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+
+            latch.await();
+        } catch (final Throwable e) {
+            System.exit(1);
+        }
+        System.exit(0);
     }
 }
